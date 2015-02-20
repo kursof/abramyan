bd5dbd72ebe67abb81aa66d97e1031d87eee218e
kursof <kurilchik.sofia@gmail.com>
2015-02-20 12:23:36 +0300

    int23

--- if/if23.cpp
+++ if/if23.cpp
@@ -10,6 +10,7 @@
 int main()
 {
 	int x1, y1, x2, y2, x3, y3;
+	int x, y;
 
 	std::cout << "введите координаты первой вершины\n";
 	std::cin >> x1 >> y1;
@@ -20,5 +21,25 @@ int main()
 	std::cout << "\nвведите координаты третьей вершины\n";
 	std::cin >> x3 >> y3;
 
+	if (x1 == x2)
+	{
+		x = x3;
+	}
+	else
+	{
+		(x1 == x3 ? x = x2:x = x1);
+	}
 
+	if (y1 == y2)
+	{
+		y = y3;
+	}
+	else
+	{
+		(y1 == y3 ? y = y2:y = y1);
+	}
+
+	std::cout << x << ", " << y << "\n";
+
+	return 0;
 } 
