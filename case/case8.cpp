882c663a0433630d253c4fd59c690dbec96923de
kursof <kurilchik.sofia@gmail.com>
2015-02-24 13:05:39 +0300

    case8-10

--- case/case8.cpp
+++ case/case8.cpp
@@ -12,13 +12,41 @@ int main()
 	std::cout << "введите дату и месяц\n";
 	std::cin >> d >> m;
 
-	switch (d, m)
+	if (d == 1)
 	{
-		case 1:
-
-		std::cout << 12;
-		break;
+		switch (m)
+		{
+			case 1:
+					d = 31, m = 12;
+					break;
+			case 2:
+			case 4:
+			case 6:
+			case 8:
+			case 9:
+			case 11:
+					d = 31, --m;
+					break;
+			case 3:
+					d = 28, --m;
+					break;
+			case 5:
+			case 7:
+			case 10:
+			case 12:
+					d = 30, --m;
+					break;
+			default:
+					std::cout << "error\n";
+					break;		
+		}
 	}
+	else
+	{
+		--d;
+	}
+
+	std::cout << d << " " << m << "\n";
 	
 	return 0;
 } 
---
+++ case/case9.cpp
@@ -0,0 +1,74 @@
+/* Даны два целых числа: D (день) и M (месяц),
+ * определяющие правильную дату невисокосного года.
+ * Вывести значения D и M для даты, следующей за указанной.
+ */
+
+#include <iostream>
+
+int main()
+{
+	int d, m;
+
+	std::cout << "введите число и месяц\n";
+	std::cin >> d >> m;
+
+	switch (m)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+				if (d == 31)
+				{
+					++m;
+					d = 1;
+				}
+				else
+				{
+					++d;
+				}
+				break;
+		case 12:
+				if (d == 31)
+				{
+					m = 1;
+					d = 1;
+				}
+				else
+				{
+					++d;
+				}
+				break;
+		case 2:
+				if (d == 28)
+				{
+					d = 1;
+					++m;
+				}
+				else
+				{
+					++d;
+				}
+				break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+				if (d == 30)
+				{
+					++m;
+					d = 1;
+				}
+				else
+				{
+					++d;
+				}
+				break;
+	}
+
+	std::cout << d << " " << m << "\n";
+
+	return 0;
+} 
